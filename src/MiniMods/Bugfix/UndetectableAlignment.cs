da0eab4b897055191e93de8a4ec1ed17580021ad
Grooki <grooki@users.noreply.github.com>
2026-03-04 20:50:32 -0800

    Add fix for undetectable bug

---
+++ src/MiniMods/Bugfix/UndetectableAlignment.cs
@@ -0,0 +1,35 @@
+﻿using Grooki.MiniMods.Settings;
+using HarmonyLib;
+using Kingmaker.UnitLogic.FactLogic;
+using System.Linq;
+
+namespace Grooki.MiniMods.Bugfix
+{
+    [HarmonyPatch(typeof(AddUndetectableAlignment.Runtime), nameof(AddUndetectableAlignment.Runtime.OnDeactivate))]
+    [SettingCategory(SettingCategory.Bugfixes)]
+    internal static class UndetectableAlignment
+    {
+        #region Properties
+
+        [Setting("Undetectable Alignment")]
+        [SettingDescription("Fixes the issue causing characters to get stuck with undetectable alignment from items.")]
+        public static bool Enabled { get; set; } = false;
+
+        #endregion Properties
+
+        #region Methods
+
+        private static void Postfix(AddUndetectableAlignment.Runtime __instance)
+        {
+            if (!Enabled) return;
+
+            var expected = __instance.Owner.Facts.List.Count(fact => fact.Active && fact.BlueprintComponents.OfType<AddUndetectableAlignment>().Any());
+            while (__instance.Owner.Alignment.Undetectable.Count > expected)
+            {
+                __instance.Owner.Alignment.Undetectable.Release();
+            }
+        }
+
+        #endregion Methods
+    }
+}
