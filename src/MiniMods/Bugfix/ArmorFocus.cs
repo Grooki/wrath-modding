8ae82e731d686bc1b054f5ce7f26f72d4b9e5c64
Grooki <grooki@users.noreply.github.com>
2026-03-16 20:07:03 -0700

    Fix armor focus handling

--- src/MiniMods/Bugfix/ArmorFocus.cs
+++ src/MiniMods/Bugfix/ArmorFocus.cs
@@ -1,30 +1,127 @@
 ﻿using Grooki.MiniMods.Settings;
+using Grooki.MiniMods.Shared;
 using HarmonyLib;
 using Kingmaker.Blueprints.Items.Armors;
+using Kingmaker.Designers.EventConditionActionSystem.Conditions;
+using Kingmaker.Designers.Mechanics.Buffs;
+using Kingmaker.EntitySystem.Entities;
 using Kingmaker.Items;
+using Kingmaker.UnitLogic.ActivatableAbilities.Restrictions;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Grooki.MiniMods.Bugfix
 {
-    [HarmonyPatch(typeof(ItemEntityArmor), nameof(ItemEntityArmor.ArmorType))]
+    [HarmonyPatch]
     [SettingCategory(SettingCategory.Bugfixes)]
     internal static class ArmorFocus
     {
         #region Properties
 
-        [Setting("Mithril Armor Feats")]
-        [SettingDescription("If enabled, mithril armor will count as its base armor type for the purposes of feats like armor focus.")]
+        [Setting("Armor Focus for Mithril Armor")]
+        [SettingDescription("If enabled, mithril armor will count as its base armor type for the purposes of armor focus and mythic armor focus.")]
         public static bool Enabled { get; set; }
 
         #endregion Properties
 
         #region Methods
 
-        public static void Postfix(ref ArmorProficiencyGroup __result, ItemEntityArmor __instance)
+        private static bool CheckArmor(UnitEntityData unit, ArmorProficiencyGroup armorProficiency)
         {
-            if (Enabled)
+            UnitBody body = unit.Body;
+            if (!RestrictionsHelper.CheckHasArmor(unit)) return false;
+
+            return body.Armor.Armor.Blueprint.ProficiencyGroup == armorProficiency;
+        }
+
+        private static bool CheckArmor(UnitEntityData unit, ArmorProficiencyGroupFlag armorProficiency)
+        {
+            UnitBody body = unit.Body;
+            if (!RestrictionsHelper.CheckHasArmor(unit)) return false;
+
+            ArmorProficiencyGroup armorProficiencyGroup = body.Armor.Armor.Blueprint.ProficiencyGroup;
+
+            return ((uint)(1 << (int)armorProficiencyGroup) & (uint)armorProficiency) != 0;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Kingmaker.Designers.Mechanics.Buffs.ArmorFocus), nameof(Kingmaker.Designers.Mechanics.Buffs.ArmorFocus.CheckArmor))]
+        private static bool FixArmorFocus(Kingmaker.Designers.Mechanics.Buffs.ArmorFocus __instance)
+        {
+            if (!Enabled) return true;
+
+            if (CheckArmor(__instance.Owner, __instance.ArmorCategory))
             {
-                __result = __instance.Blueprint.ProficiencyGroup;
+                __instance.ActivateModifier();
             }
+            else
+            {
+                __instance.DeactivateModifier();
+            }
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(HasArmorFeatureUnlock), nameof(HasArmorFeatureUnlock.CheckEligibility))]
+        private static bool FixArmorFeatureUnlock(HasArmorFeatureUnlock __instance)
+        {
+            if (!Enabled) return true;
+
+            bool hasArmorType = !__instance.FilterByBlueprintArmorTypes || RestrictionsHelper.CheckArmor(__instance.Owner, __instance.m_BlueprintArmorTypes);
+            bool hasArmorGroup = !__instance.FilterByArmorProficiencyGroup || CheckArmor(__instance.Owner, __instance.m_ArmorProficiencyGroupEntries);
+            bool hasShield = !__instance.m_DisableWhenHasShield || !__instance.Owner.Body.SecondaryHand.HasShield;
+            if (hasArmorType && hasArmorGroup && hasShield)
+            {
+                __instance.AddFact();
+            }
+            else
+            {
+                __instance.RemoveFact();
+            }
+
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(UnitArmor), nameof(UnitArmor.CheckCondition))]
+        private static bool FixArmorCondition(UnitArmor __instance, ref bool __result)
+        {
+            if (!Enabled) return true;
+
+            if (__instance.Unit == null || !__instance.Unit.TryGetValue(out var value))
+            {
+                __result = false;
+                return true;
+            }
+
+            var activeCategories = new HashSet<ArmorProficiencyGroup>();
+
+            if (RestrictionsHelper.CheckHasArmor(value))
+            {
+                activeCategories.Add(value.Body.Armor.Armor.Blueprint.ProficiencyGroup);
+            }
+            if (value.Body.PrimaryHand != null && value.Body.PrimaryHand.HasShield)
+            {
+                activeCategories.Add(value.Body.PrimaryHand.Shield.ArmorComponent.Blueprint.ProficiencyGroup);
+            }
+            if (value.Body.SecondaryHand != null && value.Body.SecondaryHand.HasShield)
+            {
+                activeCategories.Add(value.Body.SecondaryHand.Shield.ArmorComponent.Blueprint.ProficiencyGroup);
+            }
+
+            __result = true;
+
+            if (__instance.IncludeArmorCategories?.Length > 0 && __instance.IncludeArmorCategories.All(i => !activeCategories.Contains(i)))
+            {
+                __result = false;
+            }
+
+            if (__instance.ExcludeArmorCategories?.Length > 0 && __instance.ExcludeArmorCategories.Any(i => activeCategories.Contains(i)))
+            {
+                __result = false;
+            }
+
+            return false;
         }
 
         #endregion Methods
